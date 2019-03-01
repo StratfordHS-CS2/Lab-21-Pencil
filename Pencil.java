1c70b412e869ddc3327968b173e981d9d1fe4bea
Dave Avis <charles.avis@springbranchisd.com>
2019-03-01 13:48:38 -0600

    Fixed a stray }

--- Pencil.java
+++ Pencil.java
@@ -11,29 +11,28 @@ public class Pencil
     private int length;
     public boolean isSharp;
     public boolean hasEraser;
-    
+
     // This is the default constructor.  Set each of the instance variables to a reasonable default value.
     public Pencil()
     {
 
     }
-    
+
     // This is a second constructor.  Set each of the instance variables based on the parameters.
     public Pencil( String c, int l, boolean sharp, boolean eraser )
     {
 
     }
-    
+
     // Accessors Section
     // Getters
 
-    
+
     // Setters
 
-    
+
     // toString
 
-    }
 
     // when called, reduce the length by 5 (don't reduce below zero) and set isSharp to true
     public void sharpen()
